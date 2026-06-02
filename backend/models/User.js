dd66fdf847d2b56d1cfe6df3122e84d2900eb695
mattdones77 <24001392@myrp.edu.sg>
2026-06-02 21:35:53 +0800

    Update User.js
    created user.js model

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["Customer", "Shop Owner"],
+        default: "Customer"
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
